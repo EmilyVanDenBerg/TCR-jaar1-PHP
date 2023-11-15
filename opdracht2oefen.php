760406210ca3c6f6fda48e68f8e98a45900dd8c7
Emily <144031038+EmilyVanDenBerg@users.noreply.github.com>
2023-11-15 12:12:42 +0100

    opdracht 2 (opdracht in canvas ipv boek)

---
+++ opdracht2oefen.php
@@ -0,0 +1,18 @@
+<?php
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam!";
+// toegevoegde variabele
+$naam = "Emily van den Berg";
+//  Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik ben $naam";
+echo "<br>";
+echo "en ik zit op het ";
+
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam!";
+
+// Met echo kun je iets op het scherm tonen.
+echo "$schoolnaam";
+
+?>

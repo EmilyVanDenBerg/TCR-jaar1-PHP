8a9a4d0fdb75cacd39e7c736af0fde2164e2e9d8
Emily <144031038+EmilyVanDenBerg@users.noreply.github.com>
2023-11-22 13:32:28 +0100

    canvas opdracht 3 datatypes

---
+++ opdracht3.php
@@ -0,0 +1,19 @@
+<?php
+// auteur: van den Berg
+// functie: opdracht 3: datatypes (strings/getallen)
+
+// initialisatie
+$string1 = "hoi";
+$string2 = "hallo";
+$getal1 = 4;
+$getal2 = 93;
+
+// echo alles
+echo $string1;
+echo "<br>";
+echo $string2;
+echo "<br>";
+echo $getal1;
+echo "<br>";
+echo $getal2;
+?>
